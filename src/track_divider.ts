ff0bf54afeebd547d709ee333d4210753342f472
Mingzhi Zeng <zeostudio@gmail.com>
2022-01-10 21:02:46 -0800

    update copy, implement run

--- src/track_divider.ts
+++ src/track_divider.ts
@@ -3,6 +3,8 @@ import type {
   ParamDescriptor,
   PitchWidgetConfig,
   Song,
+  SongAccess,
+  Track,
   TrackSelectorWidgetConfig,
 } from 'tuneflow';
 import { TuneflowPlugin, WidgetType } from 'tuneflow';
@@ -25,8 +27,8 @@ export class TrackDivider extends TuneflowPlugin {
 
   static pluginDisplayName(): LabelText {
     return {
-      zh: '轨道分割',
-      en: 'Track Divider',
+      zh: '高低声部分离',
+      en: 'Treble Bass Separator',
     };
   }
 
@@ -55,10 +57,17 @@ export class TrackDivider extends TuneflowPlugin {
           config: {} as PitchWidgetConfig,
         },
         description: {
-          zh: '从此音高以上（包含）划分为一轨，其余音符划分为一轨。',
-          en: 'Notes higher than (including) this pitch are separated to one track. Other notes are separated to another track.',
+          zh: '将本轨道分成高低声部两轨：从此音高以上（包含）划分为高音 (Treble) 轨，其余音符划分为低音 (Bass) 轨。',
+          en: 'Track will be divided into Treble and Bass: Treble contains notes higher(including) this pitch, and Bass contains notes lower than this pitch.',
         },
       },
+    };
+  }
+
+  songAccess(): SongAccess {
+    return {
+      createTrack: true,
+      removeTrack: true,
     };
   }
 
@@ -69,7 +78,38 @@ export class TrackDivider extends TuneflowPlugin {
     inputs: { [inputName: string]: any },
     params: { [paramName: string]: any },
   ): Promise<void | { [artifactId: string]: any }> {
-    console.log(params.track);
-    console.log(params.pitch);
+    const trackId = this.getParam<string>(params, 'track');
+    let track: Track | undefined;
+    let trackIndex = -1;
+    for (trackIndex = 0; trackIndex < song.getTracks().length; trackIndex += 1) {
+      if (song.getTracks()[trackIndex].getId() === trackId) {
+        track = song.getTracks()[trackIndex];
+        break;
+      }
+    }
+    const pitch = this.getParam<number>(params, 'pitch');
+    if (!track) {
+      return;
+    }
+    const bassTrack = song.createTrack({
+      index: trackIndex,
+    });
+    const trebleTrack = song.createTrack({
+      index: trackIndex,
+    });
+    for (const note of track.getNotes()) {
+      const noteParam = {
+        pitch: note.getPitch(),
+        velocity: note.getVelocity(),
+        startTick: note.getStartTick(),
+        endTick: note.getEndTick(),
+      };
+      if (note.getPitch() >= pitch) {
+        trebleTrack.createNote(noteParam);
+      } else {
+        bassTrack.createNote(noteParam);
+      }
+    }
+    song.removeTrack(trackId);
   }
 }

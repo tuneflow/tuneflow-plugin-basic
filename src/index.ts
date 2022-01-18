48133c215163a7cb273eb1342f55dc3ab94820fa
Mingzhi Zeng <zeostudio@gmail.com>
2022-01-17 22:12:05 -0800

    add track merger

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { TempoScaler } from './tempo_scaler_plugin';
 export { TrackDivider } from './track_divider_plugin';
 export { TrackAdjust } from './track_adjust_plugin';
+export { TrackMerger } from './track_merger_plugin';

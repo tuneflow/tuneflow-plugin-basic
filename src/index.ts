b13495483a6ee16ccff25cd894da2f4584c95952
Mingzhi Zeng <zeostudio@gmail.com>
2022-01-20 18:25:48 -0800

    add remove track plugin

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { TrackAdjust } from './track_adjust_plugin';
 export { TrackMerger } from './track_merger_plugin';
 export { LayerMultiplier } from './layer_multiplier_plugin';
 export { AddDrumTrack } from './add_drum_track_plugin';
+export { TrackRemover } from './remove_track_plugin';
---
+++ src/remove_track_plugin.ts
@@ -0,0 +1,62 @@
+import type {
+  LabelText,
+  ParamDescriptor,
+  Song,
+  SongAccess,
+  TrackSelectorWidgetConfig,
+} from 'tuneflow';
+import { TuneflowPlugin, WidgetType } from 'tuneflow';
+
+export class TrackRemover extends TuneflowPlugin {
+  static providerId(): string {
+    return 'andantei';
+  }
+
+  static pluginId(): string {
+    return 'track-remover';
+  }
+
+  static providerDisplayName(): LabelText {
+    return {
+      zh: 'Andantei行板',
+      en: 'Andantei',
+    };
+  }
+
+  static pluginDisplayName(): LabelText {
+    return {
+      zh: '删除轨道',
+      en: 'Delete Track',
+    };
+  }
+  songAccess(): SongAccess {
+    return {
+      removeTrack: true,
+    };
+  }
+
+  params(): { [paramName: string]: ParamDescriptor } {
+    return {
+      trackId: {
+        displayName: {
+          zh: '轨道',
+          en: 'Track',
+        },
+        defaultValue: undefined,
+        widget: {
+          type: WidgetType.TrackSelector,
+          config: {} as TrackSelectorWidgetConfig,
+        },
+      },
+    };
+  }
+
+  async run(song: Song, params: { [paramName: string]: any }): Promise<void> {
+    const trackId = this.getParam<string>(params, 'trackId');
+    const track = song.getTrackById(trackId);
+    if (!track) {
+      throw new Error('Track not ready');
+    }
+    song.removeTrack(trackId);
+  }
+}

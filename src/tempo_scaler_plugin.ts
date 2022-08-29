3b78693bd09f530ccb85eb2052d9a40f66660db5
panacea <zaoyinji@gmail.com>
2022-08-29 11:20:28 -0700

    update tempo scaler plugin

--- src/tempo_scaler_plugin.ts
+++ src/tempo_scaler_plugin.ts
@@ -48,6 +48,7 @@ export class TempoScaler extends TuneflowPlugin {
             unit: '%',
           } as SliderWidgetConfig,
         },
+        adjustableWhenPluginIsApplied: true,
       },
     };
   }
